92c2e49f5abf0cdbceaf689b0aa824188a60d05b
Alex <alexdaniels192@gmail.com>
2014-04-03 13:04:15 -0400

    Updated to create different tables also status bar

--- xmlParser.py
+++ xmlParser.py
@@ -1,14 +1,13 @@
+"""
+Alex Daniels
+CS451
+
+"""
+
 import datetime
 import locale
 import sys
 import xml.dom.minidom
-
-itemFile = open('items.dat', 'w')
-categoryFile = open('category.dat', 'w')
-bidsFile = open('bids.dat', 'w')
-bidderFile = open('bidders.dat', 'w')
-sellerFile = open('sellers.dat', 'w')
-descriptionFile = open('descriptions.dat', 'w')
 
 def transform_dollar(dollar_str):
     """
@@ -25,106 +24,187 @@ def transform_dttm(dttm_str):
     dt = datetime.datetime.strptime(dttm_str, "%b-%d-%y %H:%M:%S")  
     return dt.isoformat(' ')
 
-def process_file(filename):
+def process_file(filename, itemFile, userFile, bidsFile, catFile):
     """
     Process one items-???.xml file.
     """
     dom = xml.dom.minidom.parse(filename)
 
     #begin the parsing madness
-    #still somewhat efficient!
     itemList = dom.getElementsByTagName('Item')
 
-    procItemTable(itemList)
-    procCatTable(itemList)
-    procBidTable(itemList)
-    procBidderTable(itemList)
-    procSellerTable(itemList)
-    procDescTable(itemList)
-    """
-    locales = item.getElementsByTagName("Location")
-        for locale in locales:
-            location = locale.childNodes[0].nodeValue
-            itemList.append("{}<>".format(location))
-        
-        countries = item.getElementsByTagName("Country")
-        for country in countries:
-            countryLoc = country.childNodes[0].nodeValue
-            itemList.append("{}<>".format(countryLoc))
-    """
+    itemTable(itemList, itemFile)
+    userTable(itemList, userFile)
+    bidTable(itemList, bidsFile)
+    categoryTable(itemList, catFile)
 
-
-
-def procItemTable(itemList):
-#find all items to fit the item table schema
+def itemTable(itemList, itemFile):
     for item in itemList:
         itemList = []
         itemId = item.getAttribute('ItemID')
+        itemId.replace('"', '')
         itemList.append("{}<>".format(itemId))
 
-        nList = item.getElementsByTagName('Name')
-        if(nList):
-            for names in nList:
-                name = names.childNodes[0].nodeValue
-                itemList.append("{}<>".format(name))
+        names = item.getElementsByTagName('Name')
+        if(names):
+            name = names[0].childNodes[0].data
+            newname = name.replace('"', '')
+            itemList.append("{}<>".format(newname))
         else:
             name = "NULL"
             itemList.append("{}<>".format(name))
 
         currPrice = item.getElementsByTagName("Currently")
         if(currPrice):
-            for prices in currPrice:
-                price = prices.childNodes[0].nodeValue
-                price = transform_dollar(price)
-                itemList.append("{}<>".format(price))
+            price = currPrice[0].childNodes[0].data
+            price = transform_dollar(price)
+            newprice = price.replace('"', '')
+            itemList.append("{}<>".format(newprice))
         else:
             price = "NULL"
             itemList.append("{}<>".format(price))
 
         buyPrices = item.getElementsByTagName("Buy_Price")
         if (buyPrices):
-            for buy in buyPrices:
-               buyPrice = buy.childNodes[0].nodeValue
-               itemList.append("{}<>".format(buyPrice))
+               buyPrice = buyPrices[0].childNodes[0].data
+               buyPrice = transform_dollar(buyPrice)
+               newbuyPrice = buyPrice.replace('"', '')
+               itemList.append("{}<>".format(newbuyPrice))
         else:
             buyPrice = "NULL"
             itemList.append("{}<>".format(buyPrice))
 
+        firstBid = item.getElementsByTagName("First_Bid")
+        if(firstBid):
+            bid = firstBid[0].childNodes[0].data
+            bid = transform_dollar(bid)
+            itemList.append("{}<>".format(bid))
+
+        numBids = item.getElementsByTagName("Number_of_Bids")
+        if(numBids):
+            bid = numBids[0].childNodes[0].data
+            itemList.append("{}<>".format(bid))
+       
         timeStart = item.getElementsByTagName("Started")
         if(timeStart):
-            for starts in timeStart:
-                start = starts.childNodes[0].nodeValue
-                start = transform_dttm(start)
-                itemList.append("{}<>".format(start))
+            start = timeStart[0].childNodes[0].data
+            start = transform_dttm(start)
+            newstart = start.replace('"', '')
+            itemList.append("{}<>".format(newstart))
         else:
             start = "NULL"
             itemList.append("{}<>".format(start))
 
         timeEnd = item.getElementsByTagName("Ends")
         if(timeEnd):
-            for ends in timeEnd:
-                end = ends.childNodes[0].nodeValue
-                end = transform_dttm(end)
-                itemList.append("{}".format(end))
+            end = timeEnd[0].childNodes[0].data
+            end = transform_dttm(end)
+            newend = end.replace('"', '')
+            itemList.append("{}<>".format(newend))
         else:
             end = "NULL"
-            itemList.append("{}".format(end))
+            itemList.append("{}<>".format(end))
 
-        """
-        locales = item.getElementsByTagName("Location")
-        for locale in locales:
-            location = locale.childNodes[0].nodeValue
-            itemList.append("{}<>".format(location))
-        
-        countries = item.getElementsByTagName("Country")
-        for country in countries:
-            countryLoc = country.childNodes[0].nodeValue
-            itemList.append("{}<>".format(countryLoc))
-        """
+        sellers = item.getElementsByTagName("Seller")
+        if(sellers):
+            userid = sellers[0].getAttribute("UserID")
+            itemList.append("{}<>".format(userid))
+        descriptions = item.getElementsByTagName("Description")
+        for desc in descriptions:
+            if(desc.hasChildNodes()):
+                description = desc.childNodes[0].data
+                itemList.append("{}".format(description))
+            else:
+                description = "NULL"
+                itemList.append("{}".format(description))
+
         write_to_file(itemList, itemFile)
         del itemList
-#END
-def procCatTable(itemList):
+
+def userTable(itemList, userFile):
+    for item in itemList:
+        userList = []
+
+        sellers = item.getElementsByTagName("Seller")
+        if(sellers):
+            userid = sellers[0].getAttribute("UserID")
+            rating = sellers[0].getAttribute("Rating")
+            userList.append("{}<>".format(userid))
+            userList.append("{}<>".format(rating))
+
+        locales = item.getElementsByTagName("Location")
+        location = locales[len(locales)-1].childNodes[0].data
+        newlocation = location.replace('"', '')
+        userList.append("{}<>".format(newlocation))
+        
+        countries = item.getElementsByTagName("Country")
+        countryLoc = countries[len(countries)-1].childNodes[0].data
+        newcountryLoc = countryLoc.replace('"', '')
+        userList.append("{}".format(newcountryLoc))
+
+        write_to_file(userList, userFile)
+        del userList
+
+        bids = item.getElementsByTagName("Bid")
+        if(bids):
+            for bid in bids:
+                userList = []
+                bidders = bid.getElementsByTagName("Bidder")
+                for bidder in bidders:
+                    bidderId = bidder.getAttribute("UserID")
+                    bidderRating = bidder.getAttribute("Rating")
+                    userList.append("{}<>".format(bidderId))
+                    userList.append("{}<>".format(bidderRating))
+
+                    locales = bidder.getElementsByTagName("Location")
+                    if(locales):
+                        locale = locales[0].getElementsByTagName("Location")
+                        location = locales[len(locales)-1].childNodes[0].data
+                        userList.append("{}<>".format(location))
+                    else:
+                        location = "Null"
+                        userList.append("{}<>".format(location)) 
+
+                    countries = bidder.getElementsByTagName("Country")
+                    if(countries):
+                        country = countries[0].getElementsByTagName("Country")
+                        countryLoc = countries[len(countries)-1].childNodes[0].data
+                        userList.append("{}".format(countryLoc))
+                    else:
+                        nCountry = "Null"
+                        userList.append("{}".format(nCountry))
+
+            write_to_file(userList, userFile)
+            del userList
+
+def bidTable(itemList, bidFile):
+    for item in itemList:
+        itemId = item.getAttribute('ItemID')
+        bids = item.getElementsByTagName("Bid")
+        if(bids):
+            for bid in bids:
+                bidList = []
+                bidList.append("{}<>".format(itemId))
+                bidders = bid.getElementsByTagName("Bidder")
+                for bidder in bidders:
+                    bidderId = bidder.getAttribute("UserID")
+                    bidList.append("{}<>".format(bidderId))
+
+                times = bid.getElementsByTagName("Time")
+                if(times):
+                    time = times[0].childNodes[0].data
+                    time = transform_dttm(time)
+                    bidList.append("{}<>".format(time))
+                amounts = bid.getElementsByTagName("Amount")
+                if(amounts):
+                    amount = amounts[0].childNodes[0].data
+                    amount = transform_dollar(amount)
+                    bidList.append("{}".format(amount))
+
+                write_to_file(bidList, bidFile)
+                del bidList
+
+def categoryTable(itemList, catFile):
 #find all items to fit the category table
     for item in itemList:
         itemId = item.getAttribute('ItemID')
@@ -133,127 +213,10 @@ def procCatTable(itemList):
         for categoryItem in catList:
             categoryList = []
             categoryList.append("{}<>". format(itemId))
-            category = categoryItem.childNodes[0].nodeValue
+            category = categoryItem.childNodes[0].data
             categoryList.append("{}".format(category))
-            write_to_file(categoryList, categoryFile)
+            write_to_file(categoryList, catFile)
             del categoryList
-#END
-def procBidTable(itemList):
-#find all items to fit bid table
-    for item in itemList:
-        bidList = []
-        itemId = item.getAttribute('ItemID')
-        bidList.append("{}<>".format(itemId))
-
-        firstBid = item.getElementsByTagName("First_Bid")
-        for bids in firstBid:
-            bid = bids.childNodes[0].nodeValue
-            bid = transform_dollar(bid)
-            bidList.append("{}<>".format(bid))
-        
-        numBids = item.getElementsByTagName("Number_of_Bids")
-        for bids in numBids:
-            bid = bids.childNodes[0].nodeValue
-            bidList.append("{}".format(bid))
-
-        write_to_file(bidList, bidsFile)
-        del bidList
-#END
-def procBidderTable(itemList):
-#find all items to fit bidder table    
-    for item in itemList:
-        bidderList = []
-        itemId = item.getAttribute('ItemID')
-        bidderList.append("{}<>".format(itemId))
-
-        bids = item.getElementsByTagName("Bid")
-        if(bids):
-            for bid in bids:
-                bidders = bid.getElementsByTagName("Bidder")
-                for bidder in bidders:
-                    bidderId = bidder.getAttribute("UserID")
-                    bidderRating = bidder.getAttribute("Rating")
-                    bidderList.append("{}<>".format(bidderId))
-                    bidderList.append("{}<>".format(bidderRating))
-
-                    locales = bidder.getElementsByTagName("Location")
-                    if(locales):
-                        for locale in locales:
-                            location = locale.childNodes[0].nodeValue
-                            bidderList.append("{}<>".format(location))
-                    else:
-                        location = "Null"
-                        bidderList.append("{}<>".format(location)) 
-
-                    countries = bidder.getElementsByTagName("Country")
-                    if(countries):
-                        for country in countries:
-                            nCountry = country.childNodes[0].nodeValue
-                            bidderList.append("{}<>".format(nCountry))
-                    else:
-                        nCountry = "Null"
-                        bidderList.append("{}<>".format(nCountry))
-
-                bidTimes = bid.getElementsByTagName("Time")
-                if(bidTimes):
-                    for bidTime in bidTimes:
-                        bTime = bidTime.childNodes[0].nodeValue
-                        bTime = transform_dttm(bTime)
-                        bidderList.append("{}<>".format(bTime))
-                else:
-                    bTime = "Null"
-                    bidderList.append("{}<>".format(bTime))
-
-                amounts = bid.getElementsByTagName("Amount")
-                if(amounts):
-                    for amount in amounts:
-                        bidPrice = amount.childNodes[0].nodeValue
-                        bidPrice = transform_dollar(bidPrice)
-                        bidderList.append("{}".format(bidPrice))
-                else:
-                    bidPrice = "NULL"
-                    bidderList.append("{}".format(bidPrice))
-        else:
-            bidderList.append("NULL")
-
-        write_to_file(bidderList, bidderFile)
-        del bidderList
-#END
-def procSellerTable(itemList):
-#find all items to fit seller table 
-    for item in itemList:
-        sellerList = []
-        itemId = item.getAttribute('ItemID')
-        sellerList.append("{}<>".format(itemId))
-
-        sellers = item.getElementsByTagName("Seller")
-        for seller in sellers:
-            userid = seller.getAttribute("UserID")
-            rating = seller.getAttribute("Rating")
-            sellerList.append("{}<>".format(userid))
-            sellerList.append("{}".format(rating))
-
-        write_to_file(sellerList, sellerFile)
-        del seller
-#END
-def procDescTable(itemList):
-#find all items to fit description table
-    for item in itemList:
-        descList = []
-        itemId = item.getAttribute('ItemID')
-        descList.append("{}<>".format(itemId))
-
-        descriptions = item.getElementsByTagName("Description")
-        for desc in descriptions:
-            if(desc.hasChildNodes()):
-                description = desc.childNodes[0].nodeValue
-                descList.append("{}".format(description))
-            else:
-                description = "NULL"
-                descList.append("{}".format(description))
-
-        write_to_file(descList, descriptionFile)
-        del descList
 #END
 def write_to_file(itemList, fileDat):
 #write to specified file
@@ -265,17 +228,43 @@ def main():
     if len(sys.argv) <= 1:
         print("Usage: python3", sys.argv[0], "[file] [file] ...")
 
-    locale.setlocale(locale.LC_ALL, 'en_US.UTF8')
+    """
+    OSX 10.9 doesn't like en_US.UTF-8 for some reason
+    But likes en_US.utf8
+    Doing either or for compatibility
+    """
+    try:
+        import locale
+        locale.setlocale(locale.LC_ALL, 'en_US.utf8')
+    except Exception:
+        try:
+            locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+        except Exception as e:
+            messages.error(request, 'An error occurred: {0}'.format(e))
 
+    itemFile = open('items.dat', 'w')
+    userFile = open('users.dat', 'w')
+    bidFile = open('bids.dat', 'w')
+    catFile = open('categories.dat', 'w')
+    
+    length = len(sys.argv[1:])
+    counter = 1
+    print("Processing: ")
     for filename in sys.argv[1:]:
-        process_file(filename)
+        percent = float(counter) / length
+        status = '#' * (int(round(percent * 20)))
+        spaces = ' ' * (20 - len(status))
+        process_file(filename, itemFile, userFile, bidFile, catFile)
+        sys.stdout.write("\r[{0}] {1}%".format(status + spaces, int(round(percent * 100))))
+        #sys.stdout.write("\rProcessing Files: %0.1f%%" % ((counter / length) * 100))
+        counter += 1
 
+    print("\n")
+    
     itemFile.close()
-    categoryFile.close()
-    bidsFile.close()
-    bidderFile.close()
-    sellerFile.close()
-    descriptionFile.close()
+    userFile.close()
+    bidFile.close()
+    catFile.close()
 
 if __name__ == "__main__":
     main()
